acfabc793ae23f147940d9ca9980e044a95ad7e8
Daniel Vance <daniel.p.vance@gmail.com>
2017-06-05 17:25:51 -0700

    Removed bug that was causing wonky collision physics.

--- daniel/p5/sketch.js
+++ daniel/p5/sketch.js
@@ -9,11 +9,12 @@
 
 
 //var ps = [];
-var count = 10;
-var pos = []
+var count = 50;
+var pos = [];
 var spd = [];
 var radii = [];
-var gravity = []
+var gravity = [];
+var mass = [];
 var friction, damping;
 var explosionRadii;
 var explosionForce;
@@ -21,31 +22,33 @@ var rSlider, gSlider, bSlider;
 var explosionX, explosionY;
 
 function setup() {
-	fill(100,255,30);
-	createCanvas(600,400);
+	fill(100, 255, 30);
+	createCanvas(600, 400);
 	friction = .875;
 	damping = .786;
 	explosionRadii = 10;
 	explosionForce = 1.4;
-	for(var i =  0; i < count; i++) {
-		pos.push(new p5.Vector(width/2 + Math.random(-10, 10), 10));
-		spd.push(new p5.Vector(-2 + Math.random()*1.3, -0 + Math.random()*2));
+	for (var i = 0; i < count; i++) {
+		pos.push(new p5.Vector( width / 2, Math.random(0, 100)));
+		// pos.push(new p5.Vector(0, 0, 0));
+		// pos.push(new p5.Vector(150, 150, 0));
+		spd.push(new p5.Vector(-2 + Math.random() * 1.3, 0 + Math.random() * 2));
 		radii.push(random(5, 10))
 		gravity.push(random(.01, .06));
-
+		mass[i] = radii[i] * 0.1;
 	}
-//	console.log(spd);
+	//	console.log(spd);
 	//background(255,128,0);
 
 	//fill(255,255,0);
 	//rect(0,0, 30,80);
-	  // create sliders
-	  rSlider = createSlider(0, 255, 100);
-	  rSlider.position(20, 20);
-	  gSlider = createSlider(0, 255, 0);
-	  gSlider.position(20, 50);
-	  bSlider = createSlider(0, 255, 255);
-	  bSlider.position(20, 80);
+	// create sliders
+	rSlider = createSlider(0, 255, 100);
+	rSlider.position(20, 20);
+	gSlider = createSlider(0, 255, 0);
+	gSlider.position(20, 50);
+	bSlider = createSlider(0, 255, 255);
+	bSlider.position(20, 80);
 }
 
 function draw() {
@@ -57,52 +60,62 @@ function draw() {
   	text("green", gSlider.x * 2 + gSlider.width, 65);
   	text("blue", bSlider.x * 2 + bSlider.width, 95);
 
-	for(var i =  0; i < count; i++) {
+	for (var i = 0; i < count; i++) {
+		ellipse(pos[i].x, pos[i].y, radii[i] * 2, radii[i] * 2);
+		checkCollisions(pos[i], spd[i], radii[i]);
+		pos[i].x += spd[i].x;
+		spd[i].y += gravity[i];
+		pos[i].y += spd[i].y;
 		for (var j = 0; j < count; j++) {
 			if (i != j) {
+				var distanceVector = p5.Vector.sub(pos[j], pos[i]);
+				var distanceVectorMagnitude = distanceVector.mag();
 				var minDistance = radii[i] + radii[j];
-				if (pos[i].dist(pos[j]) < minDistance) {
-					var distanceVector = p5.Vector.sub(pos[i], pos[j]);
-					var distanceCorrection = (minDistance - pos[i].dist(pos[j])) / 2.0;
+				if (distanceVectorMagnitude < minDistance) {
+
+					var distanceCorrection = (minDistance - distanceVectorMagnitude) / 2.0;
 					var d = distanceVector.copy();
 					var correctionVector = d.normalize().mult(distanceCorrection);
 
-					pos[j].add(correctionVector);
-					pos[i].sub(correctionVector)
+					pos[i] = pos[i].sub(correctionVector);
+					pos[j] = pos[j].add(correctionVector);
 
 					var theta = distanceVector.heading();
-					var sine = Math.sin(theta);
-					var cosine = Math.cos(theta);
+					var sine = sin(theta);
+					var cosine = cos(theta);
 
-					var posTemp = [];
-					posTemp.push(new p5.Vector());
-					posTemp.push(new p5.Vector());
+					var posTemp = [
+						new p5.Vector(),
+						new p5.Vector()
+					];
 
 					posTemp[1].x = cosine * distanceVector.x + sine * distanceVector.y;
-					posTemp[1].y = cosine * distanceVector.y + sine * distanceVector.x;
+					posTemp[1].y = cosine * distanceVector.y - sine * distanceVector.x;
 
-					var spdTemp = [];
-					spdTemp.push(new p5.Vector());
-					spdTemp.push(new p5.Vector());
+					var spdTemp = [
+						new p5.Vector(),
+						new p5.Vector()
+					];
 
 					spdTemp[0].x = cosine * spd[i].x + sine * spd[i].y;
 					spdTemp[0].y = cosine * spd[i].y - sine * spd[i].x;
 					spdTemp[1].x = cosine * spd[j].x + sine * spd[j].y;
 					spdTemp[1].y = cosine * spd[j].y - sine * spd[j].x;
 
-					var spdFinal = [];
-					spdFinal.push(new p5.Vector());
-					spdFinal.push(new p5.Vector());
+					var spdFinal = [
+						new p5.Vector(),
+						new p5.Vector()
+					];
 
-					spdFinal[0].x = spdTemp[0].x + spd[i].x;
-					if (spdTemp[0] > 0) {
+					spdFinal[0].x = ((mass[i] - mass[j]) * spdTemp[0].x + 2 * mass[j] * spdTemp[1].x) / (mass[i] + mass[j]);
+					if (spdTemp[0].y > 0) {
 						spdFinal[0].y = spdTemp[0].y + gravity[i];
 					} else {
 						spdFinal[0].y = spdTemp[0].y - gravity[i];
 					}
 
-					spdFinal[1].x = spdTemp[1].x + spd[j].x;
-					if (spdTemp[1] > 0) {
+					spdFinal[1].x = ((mass[j] - mass[i]) * spdTemp[1].x + 2 * mass[i] * spdTemp[0].x) / (mass[i] + mass[j]);
+					if (spdTemp[1].y > 0) {
 						spdFinal[1].y = spdTemp[1].y + gravity[i];
 					} else {
 						spdFinal[1].y = spdTemp[1].y - gravity[i];
@@ -123,24 +136,25 @@ function draw() {
 					posFinal[1].x = cosine * posTemp[1].x - sine * posTemp[1].y;
 					posFinal[1].y = cosine * posTemp[1].y + sine * posTemp[1].x;
 
+
+					pos[j].x = pos[i].x + posFinal[1].x;
+					pos[j].y = pos[i].y + posFinal[1].y;
+					pos[i] = pos[i].add(posFinal[0]);
+
+
 					spd[i].x = cosine * spdFinal[0].x - sine * spdFinal[0].y;
 					spd[i].y = cosine * spdFinal[0].y + sine * spdFinal[0].x;
 
 					spd[j].x = cosine * spdFinal[1].x - sine * spdFinal[1].y;
 					spd[j].y = cosine * spdFinal[1].y + sine * spdFinal[1].x;
 
-					pos[i].add(posFinal[0]);
-					pos[j].add(posFinal[1]);
+					// ellipse(pos[i].x, pos[i].y, radii[i] * 2, radii[i] * 2);
+					// ellipse(pos[j].x, pos[j].y, radii[j] * 2, radii[j] * 2);
 				}
 			}
 		}
-		ellipse(pos[i].x, pos[i].y, radii[i] * 2, radii[i] * 2);
-		checkCollisions(pos[i], spd[i], radii[i]);
-		pos[i].x += spd[i].x;
-		spd[i].y += gravity[i];
-		pos[i].y += spd[i].y;
-		//checkMouseEvents();
-		explodeParticlesNear(pos[i], spd[i], radii[i]);
+		// checkMouseEvents();
+		// explodeParticlesNear(pos[i], spd[i], radii[i]);
 	}
 }
 
